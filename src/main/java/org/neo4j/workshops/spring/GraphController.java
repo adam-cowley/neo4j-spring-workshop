4d9fff5939826d30ad53a8ffc6b3b01298573dd6
Adam Cowley <adam@adamcowley.co.uk>
2018-01-22 13:55:37 +0000

    Display list of people on GET /people

--- src/main/java/org/neo4j/workshops/spring/GraphController.java
+++ src/main/java/org/neo4j/workshops/spring/GraphController.java
@@ -1,11 +1,18 @@
 package org.neo4j.workshops.spring;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.neo4j.driver.v1.AuthTokens;
 import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.GraphDatabase;
-
+import org.neo4j.driver.v1.Record;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.StatementResult;
+import org.neo4j.driver.v1.types.Node;
 
 @RestController
 public class GraphController
@@ -22,6 +29,30 @@ public class GraphController
         this.driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic( "neo4j", "neo" ));
     }
 
+    /**
+     * When the server receives a GET request to /people, create a new session within the driver,
+     * run a read transaction and return an ArrayList of properties for each node.
+     */
+    @RequestMapping("/people")
+    public ArrayList< Map<String, Object> > index() {
+        ArrayList< Map<String, Object> > output = new ArrayList<>();
+
+        try ( Session session = driver.session() ) {
+            session.readTransaction( (tx) -> {
+                StatementResult result = tx.run("MATCH (p:Person) RETURN p");
+
+                while (result.hasNext()) {
+                    Record record = result.next();
+
+                    output.add( record.get("p").asMap() );
+                }
+
+                return tx;
+            } );
+        }
+
+        return output;
+    }
 
 
 }
